48d079a9d71fa3549fa45b622f465c2bac74dffa
doneill123 <do94482@gmail.com>
2018-10-03 20:05:37 +0100

    Splash Screen, image assets, sidebar icons,

--- app/src/main/java/com/example/danieloneill/healthyhabits/MainActivity.java
+++ app/src/main/java/com/example/danieloneill/healthyhabits/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_exercise) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_logout) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_drink) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_exercise) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_food) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_drink) {
 
         }
 
